74db34257a4a50642dadee890c7e3982839f93a6
Dmitrijs Lakacauskis <d@dmitrijs.io>
2023-02-19 22:29:31 +0200

    Day 13 small cleanup.

--- src/main/kotlin/io/dmitrijs/aoc2017/Day13.kt
+++ src/main/kotlin/io/dmitrijs/aoc2017/Day13.kt
@@ -4,20 +4,19 @@ class Day13(input: List<String>) {
     private val firewall = input.associate {
         it.substringBefore(": ").toInt() to it.substringAfter(": ").toInt()
     }
-    private val layers = 0..firewall.maxOf { it.key } + 1
 
-    fun puzzle1() = layers.filter { firewall.catches(it) }.sumOf { firewall.getValue(it) * it }
+    fun puzzle1() = firewall.keys.filter { catches(it) }.sumOf { firewall.getValue(it) * it }
 
     fun puzzle2(): Int {
         var offset = 0
 
-        while (layers.any { firewall.catches(it, offset) }) {
+        while (firewall.keys.any { catches(it, offset) }) {
             offset++
         }
 
         return offset
     }
 
-    private fun Map<Int, Int>.catches(cursor: Int, offset: Int = 0) =
-        cursor in this && (cursor + offset) % (firewall.getValue(cursor) * 2 - 2) == 0
+    private fun catches(cursor: Int, offset: Int = 0) =
+        (cursor + offset) % (firewall.getValue(cursor) * 2 - 2) == 0
 }
